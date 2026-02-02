bdae65186448f003bb88602c3347cdcba160c3ba
Obochi Kenneth <kennethobochi33@gmail.com>
2026-02-02 21:43:27 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-alert("rated 19");
+// alert("rated 19");
+
+
+    // preloader javascript
+ window.addEventListener("load", function() {
+    const preloader = document.getElementById("preloader");
+    const body = document.body;
+
+    const delay = 2000;
+
+    setTimeout(() => {
+        preloader.classList.add("hidden")
+        body.classList.remove("preloader-active");
+        setTimeout(() => preloader, remove(), 500);
+    }, delay);
+ })
